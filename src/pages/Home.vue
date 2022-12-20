67df846031751799e088d513f4c61d2ae51089b0
anmolsinghturka <anmolsinghturka@gmail.com>
2022-12-20 05:07:12 -0800

    fixed Issue-#2

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,60 +5,92 @@
         <h1>Province of British Columbia</h1>
       </b-col>
     </b-row>
-    <b-row>
-      <b-col id="selectLocationContainer" cols="3">
+    <b-row class="d-flex align-items-center">
+      <b-col id="selectLocationContainer" cols="4">
         <label for="selectLocation">Select aquifer location:</label>
-        <b-form-select id="selectLocation" v-model="selectedLocation" :options="aquiferLocations"></b-form-select>
+        <b-form-select
+          id="selectLocation"
+          v-model="selectedLocation"
+          :options="aquiferLocations"
+          v-on:change="locationChangeHandler"
+        ></b-form-select>
       </b-col>
-      <b-col id="selectedLocationContainer">
-        Selected location:<br/>
-        <b-alert show><strong>{{ selectedLocation }}</strong></b-alert>
+      <b-col
+        id="selectedLocationContainer"
+        v-if="selectedLocation == 'Please select a location'"
+      >
+        Selected location:<br />
+        <b-alert show
+          ><strong>{{ selectedLocation }}</strong></b-alert
+        >
+      </b-col>
+      <b-col id="selectedLocationDetail" v-else>
+        <b-table striped hover :items="items" :fields="fields"></b-table>
       </b-col>
     </b-row>
   </b-container>
 </template>
 
 <script>
-import AQUIFERS from '@/data/aquifers.json'
-const SELECTPLACEHOLDER = 'Please select a location'
+import AQUIFERS from "@/data/aquifers.json";
+const SELECTPLACEHOLDER = "Please select a location";
 
 export default {
-  components: {
-  },
+  components: {},
   data() {
     return {
       selectedLocation: SELECTPLACEHOLDER,
-      aquifers: AQUIFERS.results // TODO: import aquifers from an API
-    }
+      items: [],
+      fields: ["aquifer_id", "mapping_year", "name", "area", "vulnerability"],
+      aquifers: AQUIFERS.results, // TODO: import aquifers from an API
+    };
+  },
+  methods: {
+    locationChangeHandler(eve) {
+      this.items = [];
+      this.items = this.aquifers.filter((x) => x.location == eve);
+    },
   },
   computed: {
-      aquiferLocations () {
-        let locations = this.aquifers
-          // Remove null or whitespace locations (must do before sort)
-          .filter(item => item.location)
-          // Get only locations
-          .map(function(item) { 
-            let x = item.location.trim()
-            return { value: x, text: x }
-          })
-          // Sort alphabetical by location
-          // BUG: lowercase items go to bottom of list
-          .sort(function(a, b) {
-            if (a.value < b.value) { return -1 }
-            if (a.value > b.value) { return 1 }
-            return 0
-          })
-          // Return unique values (must do after sort, refactor if performance issue)
-          .filter(function(el, idx, arr) {
-            return !idx || el.value != arr[idx - 1].value
-          })
-          // Insert item
-          locations.unshift({value: SELECTPLACEHOLDER, text: SELECTPLACEHOLDER, disabled: true })
-        return locations
-      }
-  }
+    aquiferLocations() {
+      let locations = this.aquifers
+        // Remove null or whitespace locations (must do before sort)
+        .filter((item) => item.location)
+        // Get only locations
+        .map(function (item) {
+          let x = item.location.trim();
+          return { value: x, text: x };
+        })
+        // Sort alphabetical by location
+        // BUG: lowercase items go to bottom of list
+        .sort(function (a, b) {
+          let x = a.value.toLowerCase();
+          let y = b.value.toLowerCase();
+          if (x < y) {
+            return -1;
+          }
+          if (x > y) {
+            return 1;
+          }
+          return 0;
+        })
+        // Return unique values (must do after sort, refactor if performance issue)
+        .filter(function (el, idx, arr) {
+          return !idx || el.value != arr[idx - 1].value;
+        });
+      // Insert item
+      locations.unshift({
+        value: SELECTPLACEHOLDER,
+        text: SELECTPLACEHOLDER,
+        disabled: true,
+      });
+      return locations;
+    },
+  },
 };
 </script>
-
 <style scoped>
+#selectLocation {
+  width: 100%;
+}
 </style>
